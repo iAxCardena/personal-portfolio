b720fc80961f1ea71ccc87bf589c1e22dd8cacd1
Igor Alexandre <iaxcsouza@gmail.com>
2026-07-07 22:18:52 -0300

    Adding base url to vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://iaxcardena.github.io/personal-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
