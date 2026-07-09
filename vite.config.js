5715927bc84d41d2bef8c3203c6cf6819f7ae84d
Igor Alexandre <iaxcsouza@gmail.com>
2026-07-09 20:46:24 -0300

    Updating base url at vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://iaxcardena.github.io/personal-portfolio/",
+  base: '/personal-portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
