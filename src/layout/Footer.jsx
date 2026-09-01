e22985c76ce3fc405f5330ab482f0a1ac6cccc9a
Igor Alexandre <iaxcsouza@gmail.com>
2026-08-31 21:40:14 -0300

    Commenting Projects section on Footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -9,7 +9,7 @@ const socialLinks = [
 
 const footerLinks = [
   { href: "#about", label: "About" },
-  { href: "#projects", label: "Projects" },
+  // { href: "#projects", label: "Projects" },
   { href: "#experience", label: "Experience" },
   { href: "#contact", label: "Contact" },
 ];
